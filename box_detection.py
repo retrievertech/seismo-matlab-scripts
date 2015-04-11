f379addc96d871767ae74d6f76530d14e6360539
bennlich <bennlich@gmail.com>
2015-04-10 18:11:38 -0700

    Add box_detection.py, a translation of boxdetection6.m

---
+++ box_detection.py
@@ -0,0 +1,61 @@
+from timer import timeStart, timeEnd
+
+timeStart("import libs")
+import numpy as np
+from scipy import misc
+from skimage.filters import threshold_otsu
+from skimage.morphology import disk, opening
+from skimage.segmentation import find_boundaries
+from scipy.ndimage.measurements import label
+timeEnd("import libs")
+
+def get_boundary(filename, debug = False):
+  timeStart("read image")
+  grayscale_image = misc.imread(filename, flatten = True)
+  timeEnd("read image")
+  
+  timeStart("threshold image")
+  threshold_value = threshold_otsu(grayscale_image)
+  black_and_white_image = (grayscale_image > threshold_value)
+  timeEnd("threshold image")
+
+  timeStart("morphological open image")
+  filter_element = disk(4) # 17 for full-size image
+  opened_image = opening(black_and_white_image, filter_element)
+  timeEnd("morphological open image")
+
+  timeStart("invert image")
+  opened_image = np.invert(opened_image)
+  timeEnd("invert image")
+
+  timeStart("segment image into connected regions")
+  labeled_components, num_components = label(opened_image)
+  timeEnd("segment image into connected regions")
+
+  timeStart("calculate region areas")
+  # Have to cast to np.intp with ndarray.astype because of a numpy bug
+  # see: https://github.com/numpy/numpy/pull/4366
+  areas = np.bincount(labeled_components.flatten().astype(np.intp))[1:]
+  timeEnd("calculate region areas")
+
+  timeStart("calculate region boundaries")
+  image_boundaries = find_boundaries(labeled_components, connectivity=1, mode="inner", background=0)
+  timeEnd("calculate region boundaries")
+
+  timeStart("mask region of interest")
+  largest_component_id = np.argmax(areas) + 1
+  region_of_interest_mask = (labeled_components != largest_component_id)
+  region_of_interest = np.copy(image_boundaries)
+  region_of_interest[region_of_interest_mask] = 0
+  timeEnd("mask region of interest")
+
+  if debug:
+    misc.imsave("out/black_and_white_image.png", black_and_white_image)
+    misc.imsave("out/opened_image.png", opened_image)
+    misc.imsave("out/image_boundaries.png", image_boundaries)
+    misc.imsave("out/region_of_interest.png", region_of_interest)
+
+# def get_box_lines(boundary):
+
+# for testing
+get_boundary("in/dummy-seismo-small.png", debug=True)
