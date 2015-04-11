cd4e3291f9886e8cc4d5cb0fa8832a7ee9b5a69b
bennlich <bennlich@gmail.com>
2015-04-10 18:10:55 -0700

    Wrote a javascript-like timing module

---
+++ timer.py
@@ -0,0 +1,12 @@
+import sys
+from time import time
+
+timeDict = {}
+def timeStart(key):
+  timeDict[key] = time()
+  sys.stdout.write(key+": ")
+  sys.stdout.flush()
+
+def timeEnd(key):
+  sys.stdout.write(str(time() - timeDict[key])+"\n")
+  del timeDict[key]
