a3093fdfd3e41ec17377d41f6c70a504bd2fa875
bennlich <bennlich@gmail.com>
2015-04-15 17:06:24 -0700

    Add option to timer to prevent immediate logging

--- timer.py
+++ timer.py
@@ -2,11 +2,20 @@ import sys
 from time import time
 
 timeDict = {}
-def timeStart(key):
+def timeStart(key, immediate=True):
   timeDict[key] = time()
+  if immediate:
+    printStart(key)
+
+def timeEnd(key, immediate=True):
+  if not immediate:
+    printStart(key)
+  printEnd(key)
+  del timeDict[key]
+
+def printStart(key):
   sys.stdout.write(key+" ... ")
   sys.stdout.flush()
 
-def timeEnd(key):
+def printEnd(key):
   sys.stdout.write(str(time() - timeDict[key])[:6]+"s \n")
-  del timeDict[key]
